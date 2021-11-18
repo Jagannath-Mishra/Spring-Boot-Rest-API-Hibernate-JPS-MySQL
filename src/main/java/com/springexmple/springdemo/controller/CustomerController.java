0189095ef7eb391c19f1c088810500f003e58ef4
Jagannath Mishra <jagannath.mishra003@gmail.com>
2021-11-18 13:01:48 +0530

    Added producer and consumer

--- src/main/java/com/springexmple/springdemo/controller/CustomerController.java
+++ src/main/java/com/springexmple/springdemo/controller/CustomerController.java
@@ -3,17 +3,22 @@ package com.springexmple.springdemo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springexmple.springdemo.dto.CustomerData;
 import com.springexmple.springdemo.service.CustomerService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/customers")
@@ -26,7 +31,12 @@ public class CustomerController {
 	@Autowired
 	private CustomerService customerService;
 		 
-	 
+	@ApiOperation(value = "View all  customers Details", response = Iterable.class)
+	@ApiResponses(value = {
+	@ApiResponse(code = 200, message = "Successfully retrieved list"),
+	@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+	@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+	@ApiResponse(code = 404, message = "The resource you were trying to reach is not found")})
 	 @GetMapping
 	    public List < CustomerData > getCustomers() {
 	     
@@ -35,14 +45,15 @@ public class CustomerController {
 	 
 	 
 	 
-	 @GetMapping("/customer/{id}")
+	 @GetMapping(value = "/customer/{id}", produces = { "application/xml", "application/json" })
 	    public CustomerData getCustomer(@PathVariable Long id) {
 	        return customerService.getCustomerById(id);
 	    }
 	
 	 
 	 
-	 @PostMapping("/customer")
+	 //@PostMapping("/customer")
+	 @RequestMapping(value = "/customer", method= RequestMethod.POST, produces = "application/xml", consumes = "application/xml")
 	    public CustomerData saveCustomer(final @RequestBody CustomerData customerData) {
 	        return customerService.saveCustomer(customerData);
 	    }
@@ -54,7 +65,7 @@ public class CustomerController {
 	        return customerService.deleteCustomer(id);
 	    }
 	 
-	 @PutMapping("/customer/{id}")
+	 @PutMapping(value = "/customer/{id}", produces = { "application/json", "application/xml" }, consumes = MediaType.ALL_VALUE)
 	 public CustomerData updateCustomer(final @RequestBody CustomerData customerData, @PathVariable Long id){
 		 return customerService.updateCustomer(customerData, id);
 	 }
